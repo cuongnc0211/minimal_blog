3fbf39eb2840b01c6ba74c421407121c29e14f8f
cuongnc0211 <cuongnguyenfu@gmail.com>
2024-02-06 17:34:17 +0700

    load and display post in style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def default_author
+    'Cuong Nguyen'
+  end
+
+  def default_author_avatar
+    'https://res.cloudinary.com/dspnlt8se/image/upload/v1707209294/illustrations/profiles/dntgy076b5xts8jnyhhh.png'
+  end
 end
